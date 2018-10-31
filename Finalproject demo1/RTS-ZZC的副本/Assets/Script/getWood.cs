84950b7294c62ef8e4de14d2518375b03242a96c
zhouzichao <zhou.zic@husky.neu.edu>
2018-10-31 13:39:37 -0700

    3

---
+++ Finalproject demo1/RTS-ZZC的副本/Assets/Script/getWood.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getWood : MonoBehaviour {
+
+    // Use this for initialization
+    
+    private bool isgetWood = false;
+   
+    private float timer = 0f;
+
+    public int getWoodS = 20;
+    
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    
+
+        
+        if (isgetWood)
+        {
+
+			timer += Time.deltaTime;
+
+            if (timer >= 5f)
+            {
+
+                GameDataB.Wood += getWoodS;
+
+                timer = 0f;
+
+
+            }
+
+
+        }
+      
+
+
+
+
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (other.tag == "Unities" || other.tag == "isSelectedUnity")
+        {
+
+
+
+            isgetWood = true;
+
+
+        }
+
+
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Unities" || other.tag == "isSelectedUnity")
+        {
+
+            isgetWood = false;
+
+        }
+    }
+
+}
---
+++ Finalproject demo1/RTS-ZZC的副本/Assets/Script/unityAA.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unityAA : MonoBehaviour {
+
+    public int Health = 100;
+
+     bool isAttack = false;
+
+   float timer = 0.0f;
+
+
+    //public float attacktimeB = 2.0f;
+   // float AttackTime = 0.0f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Health<=0) {
+
+            Destroy(gameObject);
+
+
+        }
+
+        if (isAttack) {
+
+
+            
+			timer += Time.deltaTime;
+
+
+
+            if (timer>=3.0f) {
+
+                
+                Health -= 10;
+                timer = 0.0f;
+
+            }
+
+          
+
+
+
+        }
+
+       
+		
+	}
+
+    void OnTriggerStay(Collider collider) {
+
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {    
+
+
+
+
+        if (other.tag == "Sword") {
+
+            
+            isAttack = true;
+
+        }
+
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag=="Sword") {
+
+            isAttack = false;
+
+
+        }
+    }
+
+
+}
